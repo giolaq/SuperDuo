d654793dac457c9739f8bfac8c328c2b9cdf5247
joaobiriba <glaquidara@gmail.com>
2015-10-05 22:58:10 +0200

    Use SimpleName for LOG_TAG

--- app/src/main/java/barqsoft/footballscores/service/myFetchService.java
+++ app/src/main/java/barqsoft/footballscores/service/myFetchService.java
@@ -34,7 +34,7 @@ import barqsoft.footballscores.R;
  * Created by yehya khaled on 3/2/2015.
  */
 public class MyFetchService extends IntentService {
-    public static final String LOG_TAG = "myFetchService";
+    public static final String LOG_TAG = MyFetchService.class.getSimpleName();
 
     public static final String ACTION_DATA_UPDATED =
             "barqsoft.footballscores.service.ACTION_DATA_UPDATED";
